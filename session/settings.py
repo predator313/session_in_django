6387572422fa4fa98d19dae5d83764307ec83941
Aamir Ashraf <aamirnasa97@gmail.com>
2023-01-31 18:08:59 +0530

    saving the session in file

--- session/settings.py
+++ session/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 SESSION_COOKIE_AGE=20
+#saving session in the file
+#we need to user our own path
+SESSION_ENGINE='django.contrib.sessions.backends.file'
+SESSION_FILE_PATH=BASE_DIR/'sessionfol'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
