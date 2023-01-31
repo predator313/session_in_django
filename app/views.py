54d1f9c6f4a71f8a8ef9a4d5ff1e77e638dd1cd3
Aamir Ashraf <aamirnasa97@gmail.com>
2023-01-31 14:01:28 +0530

    get and set session working good

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def setsession(request):
+    request.session['name']='zaid'
+    return render(request,'app/setsession.html')
+
+#function for get session
+def getsession(request):
+    name=request.session['name']
+    return render(request,'app/getsession.html',{'name':name})
--- session/urls.py
+++ session/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.setsession,name='setsession'),
+    path('getsession/',views.getsession,name='getsession'),
+
 ]
