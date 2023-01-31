7e4e15e7eb8e26950b4d4339fc554588f24aa2e6
Aamir Ashraf <aamirnasa97@gmail.com>
2023-01-31 17:32:00 +0530

    adding custom expire time to the session

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def setsession(request):
@@ -7,14 +7,20 @@ def setsession(request):
 
 #function for get session
 def getsession(request):
+    if 'name' in request.session:
+        name=request.session['name']
+        return render(request,'app/getsession.html',{'name':name})
+    else:
+        return HttpResponse('hey your session is expired')
+
     # name=request.session['name']
-    name=request.session.get('name',default='guest')
-    return render(request,'app/getsession.html',{'name':name})
+    # name=request.session.get('name',default='guest')
+    # return render(request,'app/getsession.html',{'name':name})
 
 #function for deleting session
 def delsession(request):
     # if 'name' in request.session:
     #     del request.session['name']
     request.session.flush()
-    request.session.clear_expired()
+    request.session.clear_expire()
     return render(request,'app/delsession.html')
